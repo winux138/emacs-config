aed911fcac10b765835999828f486240ba0280ef
Julien Thomas <julien.thomas@external.roche.com>
2026-03-05 08:11:30 +0100

    WIP undo tree + homogenize config for emacs itself as well + some evil/undo config

--- init.el
+++ init.el
@@ -3,19 +3,9 @@
 ;; My Emacs configuration.
 ;;; Code:
 
-;; Any Customize-based settings should live in custom.el, not here.
-;; Without this emacs will dump generated custom settings in this file.
-(setq custom-file "~/.config/emacs/custom.el")
-(load custom-file 'noerror)
-
 ;; Font -- use set-face-attribute for unambiguous family + size.
 (set-face-attribute 'default nil :family "Iosevka Nerd Font Mono" :height 120 :weight 'regular)
 (set-face-attribute 'fixed-pitch nil :family "Iosevka Nerd Font Mono" :height 120 :weight 'light)
-
-(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode)
-(column-number-mode 1)
-(show-paren-mode 1)
 
 (require 'package)
 
@@ -30,6 +20,73 @@
                   ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (package-initialize)
+
+(use-package emacs
+  :ensure nil
+  :custom                                         ;; Set custom variables to configure Emacs behavior.
+  (auto-save-default nil)                         ;; Disable automatic saving of buffers.
+  (column-number-mode t)                          ;; Display the column number in the mode line.
+  (create-lockfiles nil)                          ;; Prevent the creation of lock files when editing.
+  (delete-by-moving-to-trash t)                   ;; Move deleted files to the trash instead of permanently deleting them.
+  (delete-selection-mode 1)                       ;; Enable replacing selected text with typed text.
+  (display-line-numbers-type 'relative)           ;; Use relative line numbering in programming modes.
+  (global-auto-revert-non-file-buffers t)         ;; Automatically refresh non-file buffers.
+  (history-length 25)                             ;; Set the length of the command history.
+  (indent-tabs-mode nil)                          ;; Disable the use of tabs for indentation (use spaces instead).
+  ;; (inhibit-startup-message t)                     ;; Disable the startup message when Emacs launches.
+  (initial-scratch-message "")                    ;; Clear the initial message in the *scratch* buffer.
+  (ispell-dictionary "en_US")                     ;; Set the default dictionary for spell checking.
+  (make-backup-files nil)                         ;; Disable creation of backup files.
+  (pixel-scroll-precision-mode t)                 ;; Enable precise pixel scrolling.
+  (pixel-scroll-precision-use-momentum nil)       ;; Disable momentum scrolling for pixel precision.
+  (ring-bell-function 'ignore)                    ;; Disable the audible bell.
+  (split-width-threshold 300)                     ;; Prevent automatic window splitting if the window width exceeds 300 pixels.
+  (switch-to-buffer-obey-display-actions t)       ;; Make buffer switching respect display actions.
+  (tab-width 4)                                   ;; Set the tab width to 4 spaces.
+  (treesit-font-lock-level 4)                     ;; Use advanced font locking for Treesit mode.
+  (use-dialog-box nil)                            ;; Disable dialog boxes in favor of minibuffer prompts.
+  (use-short-answers t)                           ;; Use short answers in prompts for quicker responses (y instead of yes)
+
+  :hook                                           ;; Add hooks to enable specific features in certain modes.
+  (prog-mode . display-line-numbers-mode)         ;; Enable line numbers in programming modes.
+
+  :config
+  ;; By default emacs gives you access to a lot of *special* buffers, while navigating with [b and ]b,
+  ;; this might be confusing for newcomers. This settings make sure ]b and [b will always load a
+  ;; file buffer. To see all buffers use <leader> SPC, <leader> b l, or <leader> b i.
+  (defun skip-these-buffers (_window buffer _bury-or-kill)
+    "Function for `switch-to-prev-buffer-skip'."
+    (string-match "\\*[^*]+\\*" (buffer-name buffer)))
+  (setq switch-to-prev-buffer-skip 'skip-these-buffers)
+
+  ;; Save manual customizations to a separate file instead of cluttering `init.el'.
+  ;; You can M-x customize, M-x customize-group, or M-x customize-themes, etc.
+  ;; The saves you do manually using the Emacs interface would overwrite this file.
+  ;; The following makes sure those customizations are in a separate file.
+  (setq custom-file (locate-user-emacs-file "custom-vars.el")) ;; Specify the custom file path.
+  (load custom-file 'noerror 'nomessage)                       ;; Load the custom file quietly, ignoring errors.
+
+  ;; Makes Emacs vertical divisor the symbol │ instead of |.
+  (set-display-table-slot standard-display-table 'vertical-border (make-glyph-code ?│))
+
+  :init                        ;; Initialization settings that apply before the package is loaded.
+  (tool-bar-mode -1)           ;; Disable the tool bar for a cleaner interface.
+  (menu-bar-mode -1)           ;; Disable the menu bar for a more streamlined look.
+
+  (when scroll-bar-mode
+    (scroll-bar-mode -1))      ;; Disable the scroll bar if it is active.
+
+  (global-hl-line-mode -1)     ;; Disable highlight of the current line
+  (global-auto-revert-mode 1)  ;; Enable global auto-revert mode to keep buffers up to date with their corresponding files.
+  (recentf-mode 1)             ;; Enable tracking of recently opened files.
+  (savehist-mode 1)            ;; Enable saving of command history.
+  (save-place-mode 1)          ;; Enable saving the place in files for easier return.
+  (winner-mode 1)              ;; Enable winner mode to easily undo window configuration changes.
+  (xterm-mouse-mode 1)         ;; Enable mouse support in terminal mode.
+  (file-name-shadow-mode 1)    ;; Enable shadowing of filenames for clarity.
+
+  ;; Set the default coding system for files to UTF-8.
+  (modify-coding-system-alist 'file "" 'utf-8))
 
 ;; Refresh package archive contents if not cached yet.
 ;; Without this, use-package :ensure silently fails for new packages.
@@ -49,7 +106,12 @@
    evil-want-keybinding nil
    evil-want-C-u-scroll t
    evil-want-Y-yank-to-eol t)
-  :config
+   :config
+  (evil-set-undo-system 'undo-tree)   ;; Uses the undo-tree package as the default undo system
+
+  ;; Set the leader key to space for easier access to custom commands. (setq evil-want-leader t)
+  (setq evil-leader/in-all-states t)  ;; Make the leader key available in all states.
+  (setq evil-want-fine-undo t)        ;; Evil uses finer grain undoing steps
   (evil-mode))
 
 (use-package evil-collection
@@ -92,6 +154,10 @@
 
   (leader-def
     ""    '(nil :wk "leader")
+
+    ;; Everything else
+
+    "u"   '(undo-tree-visualize :wk "undo tree")
 
     ;; Buffers
     "b"   '(:ignore t :wk "buffer")
@@ -230,8 +296,9 @@
 ;; Orderless -- hopefully a fzf-like finder/matcher
 (use-package orderless
   :custom
-  (completion-styles '(orderless flex))
+  (completion-styles '(orderless flex partial-completion basic))
   (completion-category-overrides '((file (styles partial-completion))))
+  (orderless-smart-case t)
   (completion-category-defaults nil) ;; Disable defaults, use our settings
   (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
 
@@ -263,6 +330,31 @@
   :custom
   (rustic-lsp-client 'lsp-mode))
 
+;; UNDO TREE
+;; The `undo-tree' package provides an advanced and visual way to
+;; manage undo history. It allows you to navigate and visualize your
+;; undo history as a tree structure, making it easier to manage
+;; changes in your buffers.
+(use-package undo-tree
+  :defer t
+  :hook
+  (after-init . global-undo-tree-mode)
+  :init
+  (setq undo-tree-visualizer-timestamps t
+        undo-tree-visualizer-diff t
+        ;; Increase undo limits to avoid losing history due to Emacs' garbage collection.
+        ;; These values can be adjusted based on your needs.
+        ;; 10X bump of the undo limits to avoid issues with premature
+        ;; Emacs GC which truncates the undo history very aggressively.
+        undo-limit 800000                     ;; Limit for undo entries.
+        undo-strong-limit 12000000            ;; Strong limit for undo entries.
+        undo-outer-limit 120000000)           ;; Outer limit for undo entries.
+  :config
+  ;; Set the directory where `undo-tree' will save its history files.
+  ;; This keeps undo history across sessions, stored in a cache directory.
+  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/.cache/undo"))))
+
+(use-package evil-string-inflection)
 (use-package magit)
 (use-package eat)
 (use-package direnv
